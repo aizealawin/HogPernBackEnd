cd55d1e75862504977220bb0d346fd43b3f76b80
aizealawin <ktmudalige@gmail.com>
2022-11-21 12:05:31 -0500

    heroku deployment

--- models/course.js
+++ models/course.js
@@ -6,6 +6,7 @@ module.exports = (sequelize, DataTypes) => {
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
+     * hi world
      */
     static associate(models) {
       Course.hasMany(models.Student, {
