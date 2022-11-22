7101bd2505732cc062858fe9f0cdb9e1665e3a99
Edward Yuabov <yuaboved@gmail.com>
2022-11-22 08:11:57 -0500

    added authorization backend

--- models/user.js
+++ models/user.js
@@ -16,8 +16,26 @@ module.exports = (sequelize, DataTypes) => {
   }
   User.init(
     {
-      userName: DataTypes.STRING,
-      password: DataTypes.STRING
+      userName: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true
+        }
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true
+        }
+      }
     },
     {
       sequelize,
